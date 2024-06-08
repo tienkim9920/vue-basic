6b637a1bb796fdff8182070f8e8a00aaf7e44586
Tien Kim <tienkim9920@gmail.com>
2024-06-08 11:31:32 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <script setup>
+import { computed, onMounted, ref } from 'vue';
+
+const users = ref([]);
+const txtSearch = ref('');
+
+onMounted(() => {
+  (async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    users.value = data;
+  })();
+});
+
+const filterUsers = computed(() => {
+  return users.value.filter(item => item.name.toUpperCase().indexOf(txtSearch.value.toUpperCase()) !== -1 ||
+    item.email.toUpperCase().indexOf(txtSearch.value.toUpperCase()) !== -1);
+})
 </script>
 
 <template>
   <main style="color: #000; padding: 2rem;">
-    <input type="text" placeholder="Enter Search!">
+    <input type="text" placeholder="Enter Search Here!" v-model="txtSearch" />
     <div class="group-card">
-      <div class="card-item">
-        <h2>Ninedev</h2>
-        <i>Hello</i>
-      </div>
-      <div class="card-item">
-        <h2>Ninedev</h2>
-        <i>Hello</i>
+      <div class="card-item" v-for="user in filterUsers">
+        <h2>{{ user.name }}</h2>
+        <i>{{ user.email }}</i>
       </div>
     </div>
   </main>
@@ -23,7 +36,7 @@ input {
   padding: .6rem 1.2rem;
   border-radius: 50px;
   border: none;
-  outline: linen;
+  outline: none;
   background: #ededed;
 }
 
@@ -36,11 +49,11 @@ input {
 
 .card-item {
   border-radius: 10px;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.15);
-  padding: .8rem 1rem;
   background: #324558;
   color: #fff;
   cursor: pointer;
+  padding: .8rem 1rem;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.15);
 }
 
 .card-item:hover {
